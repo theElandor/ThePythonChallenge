0fc6bf092b7a483fd47352d8831b0a0f1485b127
theElandor <69966243+theElandor@users.noreply.github.com>
2020-10-03 00:17:44 +0200

    Add files via upload

---
+++ 0_se_negativo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+int main(){
+	//0 negativo, 1 positivo
+	int k ,c;
+	cin>>k;
+	c = k +abs(k);
+	cout<<c/abs(k)/2;		
+	
+	
+	return 0;
+}
